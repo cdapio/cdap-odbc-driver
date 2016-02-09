695d7751bbf3a6ab9705b141dd619fc7da811fa4
ityshko <ityshko@cybervisiontech.com>
2016-02-09 16:13:59 +0200

    Added data display to scripts

--- CdapObcs.TestsManual/test_double.sh
+++ CdapObcs.TestsManual/test_double.sh
@@ -4,3 +4,4 @@ create stream test_double
 truncate stream test_double
 load stream test_double test_double.csv
 set stream format test_double csv 'col_id int, col_double double, col_expected string'
+execute 'select col_id, col_double, col_expected from stream_test_double'
--- CdapObcs.TestsManual/test_float.sh
+++ CdapObcs.TestsManual/test_float.sh
@@ -4,3 +4,4 @@ create stream test_float
 truncate stream test_float
 load stream test_float test_float.csv
 set stream format test_float csv 'col_id int, col_float float, col_expected string'
+execute 'select col_id, col_float, col_expected from stream_test_float'
