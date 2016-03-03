0ae35564f3576c1d179766872e5b8eee5e6d89b7
ityshko <ityshko@cybervisiontech.com>
2016-03-03 20:22:38 +0200

    Updated test select request for STRING

--- CdapObcs.TestsManual/test_setup.sh
+++ CdapObcs.TestsManual/test_setup.sh
@@ -41,7 +41,7 @@ create stream test_string
 truncate stream test_string
 load stream test_string test_string.csv
 set stream format test_string csv 'col_id int, col_string string, col_expected string'
-execute 'select col_id, col_string, col_expected from stream_test_string'
+execute 'select col_id, substr(col_string,0,100), length(col_string), col_expected from stream_test_string'
 
 create stream test-bad-name
 truncate stream test-bad-name
