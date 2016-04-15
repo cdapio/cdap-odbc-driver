8c1366e748d91dc7267669864a49dbc43b4ec20f
ityshko <iryna.tyshko@gmail.com>
2016-04-15 20:40:14 +0300

    Delete test_long.sh

--- CdapOdbc.TestsManual/test_long.sh
+++
@@ -1,6 +0,0 @@
-create namespace testspace
-use namespace testspace
-create stream test_long
-truncate stream test_long
-load stream test_long test_long.csv
-set stream format test_long csv 'col_id int, col_long long, col_expected string'
